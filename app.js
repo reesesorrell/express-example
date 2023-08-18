606864116484cffafc585f993b95770cd8b16512
Reese Sorrell <joreesesorrell@gmail.com>
2023-08-18 09:58:41 -0500

    Add base jade layout

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const wiki = require('./routes/wiki.js')
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/wiki', wiki);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
